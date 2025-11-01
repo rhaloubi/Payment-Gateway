81a746d2a67f3a5bed05b3ff3c5947adcfc54dd1
rhaloubi <redahaloubi8@gmail.com>
2025-11-01 14:24:53 +0100

    add auth service init

---
+++ auth-service/internal/api/routes.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	//"github.com/rhaloubi/go-learning/db/controllers"
+)
+
+var R *gin.Engine
+
+func Routes() {
+	R = gin.Default()
+	// Define your routes here
+	R.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "home page",
+		})
+	})
+	/*R.POST("/accounts", controllers.CreateAccount)
+	R.GET("/accounts", controllers.GetAccounts)
+	R.GET("/accounts/:id", controllers.GetAccountByID)
+	R.PUT("/accounts/:id", controllers.UpdateAccount)
+	R.PATCH("/accounts/:id", controllers.UpdateAccount)
+	R.DELETE("/accounts/:id", controllers.DeleteAccount)*/
+}
