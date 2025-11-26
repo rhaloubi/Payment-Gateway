5ffb5f57ff4ed2bc3e0068680b64a77c44bb0fb3
rhaloubi <redahaloubi8@gmail.com>
2025-11-26 18:29:26 +0100

    correct port errer

--- auth-service/internal/util/initGrpc.go
+++ auth-service/internal/util/initGrpc.go
@@ -3,22 +3,23 @@ package util
 import (
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 )
 
 // InitGRPC initializes and returns the gRPC server (without registering services)
 func InitGRPC() *grpc.Server {
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":"+os.Getenv("GRPC_PORT"))
 	if err != nil {
-		log.Fatalf("❌ Failed to listen on port 50051: %v", err)
+		log.Fatalf("❌ Failed to listen on port %s: %v", os.Getenv("GRPC_PORT"), err)
 	}
 
 	grpcServer := grpc.NewServer()
 
 	// Start serving in a goroutine
 	go func() {
-		log.Println("🚀 gRPC server running on :50051")
+		log.Printf("🚀 gRPC server running on :%s", os.Getenv("GRPC_PORT"))
 		if err := grpcServer.Serve(lis); err != nil {
 			log.Fatalf("❌ Failed to serve gRPC: %v", err)
 		}
