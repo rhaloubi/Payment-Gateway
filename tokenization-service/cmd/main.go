1e64ffe3ee3be6ac17b5c22aa15d6cf05245f17d
Reda Haloubi <redahaloubi8@gmail.com>
2025-12-09 00:51:03 +0100

    remove the http server from tokenization service

--- tokenization-service/cmd/main.go
+++ tokenization-service/cmd/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"context"
-	"net/http"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
-	"time"
 
 	"github.com/rhaloubi/payment-gateway/tokenization-service/inits"
 	"github.com/rhaloubi/payment-gateway/tokenization-service/inits/logger"
-	"github.com/rhaloubi/payment-gateway/tokenization-service/internal/api"
 	"github.com/rhaloubi/payment-gateway/tokenization-service/internal/grpc"
 	"github.com/rhaloubi/payment-gateway/tokenization-service/internal/util"
 	pb "github.com/rhaloubi/payment-gateway/tokenization-service/proto"
@@ -23,7 +18,6 @@ func init() {
 	inits.InitDB()
 	inits.InitRedis()
 	logger.Init()
-	api.SetupRoutes(inits.R)
 }
 
 func main() {
@@ -41,22 +35,6 @@ func main() {
 		}
 	}()
 
-	httpServer := &http.Server{
-		Addr:    ":" + os.Getenv("PORT"),
-		Handler: inits.R,
-	}
-
-	// Run HTTP server in goroutine
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		logger.Log.Info("🚀 HTTP (Gin) server running on :" + os.Getenv("PORT"))
-		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Log.Error("HTTP server error", zap.Error(err))
-		}
-	}()
-
 	// Shutdown channel
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
@@ -64,23 +42,11 @@ func main() {
 	<-stop
 	logger.Log.Warn("🛑 Shutting down gracefully...")
 
-	// Shutdown HTTP server with timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := httpServer.Shutdown(ctx); err != nil {
-		logger.Log.Error("HTTP server shutdown error", zap.Error(err))
-	} else {
-		logger.Log.Info("🧹 HTTP server stopped.")
-	}
-
 	// Shutdown gRPC server
 	if grpcServer != nil {
 		logger.Log.Info("🧹 Stopping gRPC server...")
 		grpcServer.GracefulStop()
 	}
-
-	// Wait for HTTP goroutine to finish
-	wg.Wait()
 
 	// Close Redis connection
 	if err := inits.RDB.Close(); err != nil {
